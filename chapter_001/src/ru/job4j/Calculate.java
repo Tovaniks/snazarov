4a9a8d89b9279db02d47b7286678ecebe9dd0876
Sergey Nazarov <tovaniks@yandex.ru>
2018-01-20 23:40:41 +0300

    1.3. JavaDoc. Code convetion[#4087]

--- chapter_001/src/ru/job4j/Calculate.java
+++ chapter_001/src/ru/job4j/Calculate.java
@@ -1,6 +1,23 @@
 package ru.job4j;
 
+
+/**
+ *Class Calculate.
+ *@author Sergey Nazarov
+ *@version $Id$
+ *@since 2018.01.20
+
+*/
+
+
 public class Calculate {
+	
+	/**
+	 *Main.
+	 *@param args - args
+	
+	*/
+	
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *@author Sergey Nazarov
+ *@version $Id$
+ *@since 2018.01.01
+*/
+
+package ru.job4j;
