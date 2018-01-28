aab121d232827d4016372e42744481a8f349b209
Sergey Nazarov <tovaniks@yandex.ru>
2018-01-28 14:01:12 +0300

    2.3 Элементарный калькулятор[#185]

--- chapter_001/src/Main/Java/ru/job4j/Calculate.java
+++ chapter_001/src/Main/Java/ru/job4j/Calculate.java
@@ -2,22 +2,34 @@ package ru.job4j;
 
 
 /**
- *Class Calculate.
- *@author Sergey Nazarov
- *@version $Id$
- *@since 2018.01.20
+ * Class Calculate.
+ *
+ * @author Sergey Nazarov
+ * @version $Id$
+ * @since 2018.01.20
  */
 
 
 public class Calculate {
 
-	/**
-	 * Method echo.
-	 * @param name Your name.
-	 * @return Echo plus your name.
-	 */
-	public String echo(String name) {
-		return "Echo, echo, echo : " + name;
-	}
+    /**
+     * Main.
+     *
+     * @param args - args
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    /**
+     * Method echo.
+     *
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 
 }
