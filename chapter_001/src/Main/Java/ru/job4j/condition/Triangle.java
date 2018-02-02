2bba0756637d55432745f759c05537a06d2dd3c1
Sergey Nazarov <tovaniks@yandex.ru>
2018-02-02 18:22:59 +0300

    Правки по результатам ревью кода.

--- chapter_001/src/Main/Java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/Main/Java/ru/job4j/condition/Triangle.java
@@ -54,9 +54,6 @@ public class Triangle {
      * @return возвращает результат проверки
      */
     private boolean exist(double ab, double ac, double bc) {
-        if (ab + ac - bc <= 0 || ab + bc - ac <= 0 || ac + bc - ab <= 0) {
-            return false;
-        }
-        return true;
+        return !(ab + ac <= bc || ab + bc <= ac || ac + bc <= ab );
     }
 }
