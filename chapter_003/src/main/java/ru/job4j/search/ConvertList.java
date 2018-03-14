55e9445a7c139bd1b1370176d5503b151df3c841
Sergey Nazarov <tovaniks@yandex.ru>
2018-03-14 19:28:03 +0300

    3. Конвертация двумерного массива в ArrayList и наоборот [#10035]

---
+++ chapter_003/src/main/java/ru/job4j/search/ConvertList.java
@@ -0,0 +1,49 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ConvertList.
+ *
+ * @author Sergey Nazarov
+ * @version $Id$
+ * @since 2018.03.14
+ */
+public class ConvertList {
+    /**
+     * Конвертируем массив в List
+     *
+     * @param array массив
+     * @return List
+     */
+    public List<Integer> toList(int[][] array) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] anArray : array) {
+            for (int anAnArray : anArray) {
+                result.add(anAnArray);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Конвертируем List в список с заданным кол-вом колонок. Если в List не хватает элементов, заполняется 0
+     *
+     * @param list список
+     * @param rows кол-во колонок
+     * @return массив
+     */
+    public int[][] toArray(List<Integer> list, int rows) {
+        int columns = (int) Math.ceil(list.size() / (double) rows);
+        int[][] result = new int[columns][rows];
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                result[i][j] = i * rows + j < list.size() ? list.get(i * rows + j) : 0;
+            }
+        }
+        return result;
+    }
+
+
+}
---
+++ chapter_003/src/test/java/ru/job4j/search/ConvertListTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class ConvertListTest {
+    @Test
+    public void whenConvertArrayToList() {
+        ConvertList converter = new ConvertList();
+        int[][] expected = new int[][]{{0, 1, 1}, {0, 2}, {3, 4, 2}};
+        List<Integer> result = converter.toList(expected);
+        int index = 0;
+        for (int[] anExpected : expected) {
+            for (int anAnExpected : anExpected) {
+                assertThat(result.toArray()[index], is(anAnExpected));
+                index++;
+            }
+        }
+    }
+
+    @Test
+    public void whenConvertListToArrayThreeOnFour() {
+        ConvertList converter = new ConvertList();
+        List<Integer> input = new ArrayList<>();
+        input.add(1);
+        input.add(2);
+        input.add(3);
+        input.add(4);
+        input.add(0);
+        input.add(10);
+        input.add(23);
+        input.add(1);
+        input.add(4);
+        input.add(10);
+        int[][] result = converter.toArray(input, 3);
+        int[][] expected = {{1, 2, 3}, {4, 0, 10}, {23, 1, 4}, {10, 0, 0}};
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                assertThat(result[i][j], is(expected[i][j]));
+            }
+        }
+    }
+}
